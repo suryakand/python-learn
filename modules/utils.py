41450c9ba4a3caf0e800d401c8f54ae819b9dbc0
Suryakand <suryakand@gmail.com>
2019-10-02 10:07:21 -0400

    Create utils.py

---
+++ modules/utils.py
@@ -0,0 +1,5 @@
+def public_function(name):
+    print('Hello {}! this is a public function'.format(name))
+
+def _private_function(name):
+    print('Hello {}! this is a private function'.format(name))
